7c4a44b12ace8e2fb82335ef2d0b1a9f56304b82
Harshit Kumar <harshitkumarhk1290@gmail.com>
2026-06-08 15:17:29 +0530

    feat: implemented websocket to join room

--- backend/routers/GameRoutes.go
+++ backend/routers/GameRoutes.go
@@ -4,9 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/harshitkumar7525/RapidQuiz/backend/controllers"
 	"github.com/harshitkumar7525/RapidQuiz/backend/middlewares"
+	"github.com/harshitkumar7525/RapidQuiz/backend/websocket"
 )
 
 func RegisterGameRoutes(server *gin.Engine) {
 	server.POST("/games/create", middlewares.AuthMiddleware, controllers.StartGame)
 	server.POST("/games/join", controllers.JoinGame)
+	server.GET("/ws/:roomCode", websocket.HandleWebSocket)
 }
